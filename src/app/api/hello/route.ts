274ba5c241b72ddc6dc7d1a91807295d54e4520a
emoryhubbard <edhthe4th@gmail.com>
2024-02-12 01:23:00 -0500

    Removed the other export keyword

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -31,7 +31,7 @@ import { NextRequest, NextResponse } from "next/server";
         },
     });*/
   //}
-  export const corsHeaders = {
+  const corsHeaders = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
